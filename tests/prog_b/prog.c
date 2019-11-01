44de394529d693ee71ecbc6bfdf67f8d3eb28457
神崎航 <kanzwataru@gmail.com>
2019-10-31 22:13:12 -0400

    * add prog_b

--- tests/prog_b/prog.c
+++ tests/prog_b/prog.c
@@ -1,6 +1,5 @@
 #include "structs.h"
 #include "genetics.h"
-#include "structs_genetics.gen.h"
 #include <stdio.h>
 #include <assert.h>
 
@@ -19,36 +18,44 @@ static struct Bar b = {
     15.1337f
 };
 
+static struct TypeInfo structs_gen[GEN_MAX];
+
 static void print_bar(struct Bar *bar, int indents)
 {
     int i, z;
 
-    #define INDENT() for(z = 0; z < indents; ++i) { printf("\t"); }
+    #define INDENT() for(z = 0; z < indents; ++z) { printf("\t"); }
 
     INDENT(); printf("start Bar\n");
     for(i = 0; i < 8; ++i) {
-        INDENT(); printf("\tz[%d]: %d\n", bar->z[i]);
+        INDENT(); printf("\tz[%d]: %d\n", i, bar->z[i]);
     }
     INDENT(); printf("\tx: %f\n", bar->x);
-    INDENT(); printf("end Bar"\n);
+    INDENT(); printf("end Bar\n");
 
     #undef INDENT
 }
 
 static void print_foo(struct Foo *foo)
 {
-    fprintf("start Foo\n");
-    print_bar(foo->bar, 1);
-    fprintf("\tb: %f\n", foo->b);
-    fprintf("\tc: %f\n", foo->c);
-    fprintf("\ta: %d\n", foo->a);
-    fprintf("end Foo\n\n");
+    printf("start Foo\n");
+    print_bar(&foo->bar, 1);
+    printf("\tb: %f\n", foo->b);
+    printf("\tc: %f\n", foo->c);
+    printf("\ta: %d\n", foo->a);
+    printf("end Foo\n\n");
 }
 
 int main(int argc, char **argv)
 {
     int ret;
     FILE *fp;
+
+    /* get our own genetics */
+    fp = fopen("prog_b/structs.h", "r");
+    assert(fp);
+    genetics_parse(fp, structs_gen, GEN_MAX);
+    fclose(fp);
 
     /* write out own genetics */
     fp = fopen("prog_b_gen.bin", "wb");
@@ -65,7 +72,7 @@ int main(int argc, char **argv)
 
     /* print own data */
     print_foo(&f);
-    print_bar(&b);
+    print_bar(&b, 0);
 
     return 0;
 }
